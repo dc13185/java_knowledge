622a7d3cacfed64d644d3a4522338c43046d95e4
dongchao <1318533144@qq.com>
2021-03-23 14:38:26 +0800

    Semaphor 代码验证

---
+++ src/main/java/Lock/SemaphoreDemo.java
@@ -0,0 +1,39 @@
+package Lock;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author Dong.Chao
+ * @Classname SemaphoreDemo
+ * @Description Semaphore 代码验证
+ * @Date 2021/3/23 14:07
+ * @Version V1.0
+ */
+public class SemaphoreDemo {
+    private static Semaphore semaphore = new Semaphore(5);
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            try {
+                final int temp = i;
+                semaphore.acquire();
+                new Thread(() -> {
+                    System.out.println(Thread.currentThread().getName()+"进来了！");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"出来了！");
+                    semaphore.release();
+                },"Thread"+temp).start();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+            }
+
+        }
+    }
+
+}
