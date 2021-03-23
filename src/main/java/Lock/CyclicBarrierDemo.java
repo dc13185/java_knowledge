6a8502595bcc1347769e6af0328a2c5be517b753
dongchao <1318533144@qq.com>
2021-03-23 11:42:47 +0800

    CyclicBarrier代码验证

---
+++ src/main/java/Lock/CyclicBarrierDemo.java
@@ -0,0 +1,41 @@
+package Lock;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author Dong.Chao
+ * @Classname CyclicBarrierDemo
+ * @Description CyclicBarrier demo
+ * @Date 2021/3/23 9:46
+ * @Version V1.0
+ */
+public class CyclicBarrierDemo {
+
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            final int temp = i;
+            new Thread(() -> {
+                try {
+                    String  threadName = Thread.currentThread().getName();
+                    System.out.println(threadName + "进来了!");
+                    cyclicBarrier.await();
+                    Thread.sleep(1000);
+                    System.out.println(threadName + "进入游戏");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            },"线程"+temp).start();
+        }
+
+
+
+
+    }
+
+}
