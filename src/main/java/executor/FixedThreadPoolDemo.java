22b8bef9fc43d2f616efa7e5201310152ef11cba
dongchao <1318533144@qq.com>
2021-03-25 16:56:57 +0800

    线程池Demo

---
+++ src/main/java/executor/FixedThreadPoolDemo.java
@@ -0,0 +1,31 @@
+package executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Dong.Chao
+ * @Classname ExecutorsDemo
+ * @Description
+ * @Date 2021/3/25 16:40
+ * @Version V1.0
+ */
+public class FixedThreadPoolDemo {
+    public static void main(String[] args) {
+        // newFixedThreadPool 创建固定线程数量的线程池
+        //  Executors.newSingleThreadExecutor(); 创建一个单线程的线程池
+        // Executors.newCachedThreadPool() 创建一个可扩容线程的线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try {
+            for (int i = 0; i < 9; i++) {
+                executorService.execute(() -> {
+                    System.out.println(Thread.currentThread().getName()+"执行线程任务");
+                });
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            executorService.shutdown();
+        }
+    }
+}
