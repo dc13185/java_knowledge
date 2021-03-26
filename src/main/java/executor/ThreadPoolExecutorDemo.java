c53f413e6b328495ed3c3fd458e41dd422e4f9a9
dongchao <1318533144@qq.com>
2021-03-26 16:47:37 +0800

    ThreadPoolExecutor 的四种拒绝策略详解

--- src/main/java/executor/ThreadPoolExecutorDemo.java
+++ src/main/java/executor/ThreadPoolExecutorDemo.java
@@ -1,5 +1,9 @@
 package executor;
 
+import com.sun.org.apache.xerces.internal.dom.AbortException;
+
+import java.util.concurrent.*;
+
 /**
  * @author Dong.Chao
  * @Classname ThreadPoolExecutorDemo
@@ -8,4 +12,38 @@ package executor;
  * @Version V1.0
  */
 public class ThreadPoolExecutorDemo {
+    static ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(2,3,5L
+            , TimeUnit.SECONDS, new LinkedBlockingQueue<>(5), Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
+    /**
+     *      四种线程池拒绝策略
+     *      AbortPolicy: 抛出一个类型为 RejectedExecutionException 的 RuntimeException异常
+     *      CallerRunsPolicy:让提交任务的线程执行任务，在这段期间（提交线程运行提交任务），提交任务的线程被占用，无法继续提交新的线程。
+     *      DiscardPolicy: 将新任务提交后直接丢掉
+ *          DiscardOldestPolicy: 直接丢弃存活时间最长的任务。
+     *
+     *
+     *
+     *
+     *
+     * */
+
+
+
+
+    public static void main(String[] args) {
+        try {
+            for (int i = 1; i <= 9; i++) {
+                THREAD_POOL_EXECUTOR.execute(new Thread(() -> {
+                    System.out.println(Thread.currentThread().getName()+ "业务处理中");
+                }));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            THREAD_POOL_EXECUTOR.shutdown();
+        }
+
+
+    }
+
 }
